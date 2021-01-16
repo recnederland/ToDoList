5a3d60299ede9063469718796923d450ee9db7a5
Recep <recbay@gmail.com>
2021-01-16 17:30:15 +0100

    deneme3 heroku

--- index.js
+++ index.js
@@ -67,3 +67,11 @@ app.listen(port, function(){
   console.log("port numarasi : " + port);
 });
 app.listen(5000);
+
+let port = process.env.PORT;
+if(port == "" || port == null){
+  port = 5000;
+}
+app.listen(port, function(){
+  console.log("port numarasi : " + port);
+});
